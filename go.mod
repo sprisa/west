b19ce9c7cb664280b6c456af9b34b7c99381cd52
Gabe M <hey@gabe.mx>
2025-10-12 17:46:25 -0600

    add nebula

--- go.mod
+++ go.mod
@@ -1,3 +1,46 @@
 module github.com/sprisa/west
 
 go 1.25.2
+
+require (
+	github.com/sirupsen/logrus v1.9.3
+	github.com/slackhq/nebula v1.9.7
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/armon/go-radix v1.0.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cyberdelia/go-metrics-graphite v0.0.0-20161219230853-39f87cc3b432 // indirect
+	github.com/flynn/noise v1.1.0 // indirect
+	github.com/gaissmai/bart v0.11.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/miekg/dns v1.1.61 // indirect
+	github.com/nbrownus/go-metrics-prometheus v0.0.0-20210712211119-974a6260965f // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8 // indirect
+	github.com/vishvananda/netlink v1.2.1-beta.2 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090 // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/term v0.23.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
+	golang.zx2c4.com/wireguard v0.0.0-20230325221338-052af4a8072b // indirect
+	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ nebula.go
@@ -0,0 +1,144 @@
+package nebula
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/sprisa/west/config"
+
+	"github.com/sirupsen/logrus"
+	"github.com/slackhq/nebula"
+	nebulaCfg "github.com/slackhq/nebula/config"
+	"github.com/slackhq/nebula/overlay"
+	"github.com/slackhq/nebula/util"
+	"gopkg.in/yaml.v3"
+)
+
+// A version string that can be set with
+//
+//	-ldflags "-X main.Build=SOMEVERSION"
+//
+// at compile-time.
+var Build string = "west v0.0.1"
+
+type Control = nebula.Control
+type NebulaConfigCtrl = nebulaCfg.C
+
+// TODO: Add add an `onReady` and wait for Nebula to be fully up.
+// e.g. after "handshake message received"
+type ServerArgs struct {
+	// Custom Logging
+	Log *logrus.Logger
+	// Nebula config
+	Config *config.Config
+	// Hook called after Nebula server starts
+	OnStart func(*Control)
+	// Hook called before Nebula shuts down.
+	// Will block until function returns.
+	OnShutdown    func()
+	deviceFactory overlay.DeviceFactory
+}
+
+type Server struct {
+	Ctrl *Control
+	args *ServerArgs
+}
+
+func InitServer(args *ServerArgs) (*Server, error) {
+	log := args.Log
+	if log == nil {
+		return nil, errors.New("expected Log arg")
+	}
+	nebulaConfig := args.Config
+	if nebulaConfig == nil {
+		return nil, errors.New("expected west config")
+	}
+
+	c, err := CreateNebulaConfigCtrl(nebulaConfig, log)
+	if err != nil {
+		return nil, err
+	}
+
+	return InitServerWithConfigCtrl(args, c)
+}
+
+func InitServerWithConfigCtrl(args *ServerArgs, c *NebulaConfigCtrl) (*Server, error) {
+	log := args.Log
+	if log == nil {
+		return nil, errors.New("expected Log arg")
+	}
+	nebulaConfig := args.Config
+	if nebulaConfig == nil {
+		return nil, errors.New("expected west config")
+	}
+
+	port := nebulaConfig.Listen.Port
+
+	// TODO: Ensure mknod is still upstream
+
+	ctrl, err := nebula.Main(c, false, Build, log, args.deviceFactory, nil)
+
+	if err != nil {
+		switch v := err.(type) {
+		case *util.ContextualError:
+			v.Log(log)
+			return nil, v.Unwrap()
+		default:
+			// TODO: Move this port error decoration up into harbor
+			return nil, fmt.Errorf("nebula error on port %d: %w", port, err)
+		}
+	}
+
+	return &Server{Ctrl: ctrl, args: args}, nil
+}
+
+func CreateNebulaConfigCtrl(cfg *config.Config, log *logrus.Logger) (*NebulaConfigCtrl, error) {
+	nebulaYaml, err := configToYaml(cfg)
+	if err != nil {
+		return nil, err
+	}
+	nebulaYamlStr := string(nebulaYaml)
+	println("CONFIG")
+	println(nebulaYamlStr)
+
+	c := nebulaCfg.NewC(log)
+	// Can use ReloadConfigString to handle static-host-map changes
+	// c.ReloadConfigString()
+	// TODO: Could just turn the struct into a record map
+	// or commit the types upstream.
+	err = c.LoadString(nebulaYamlStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load nebula config: %w", err)
+	}
+
+	return c, nil
+}
+
+func configToYaml(cfg *config.Config) ([]byte, error) {
+	return yaml.Marshal(cfg)
+}
+
+func (s *Server) Listen(ctx context.Context) error {
+	log := s.args.Log
+	port := s.args.Config.Listen.Port
+	// Start Nebula Server
+	s.Ctrl.Start()
+	// Wait for OnStart hook
+	if s.args.OnStart != nil {
+		s.args.OnStart(s.Ctrl)
+	}
+	// Wait for server to be stopped (context cancelled)
+	<-ctx.Done()
+	// Wait for OnShutdown hook
+	if s.args.OnShutdown != nil {
+		s.args.OnShutdown()
+	}
+	log.Infof("Shutting down nebula server on port %d \n", port)
+	s.Ctrl.Stop()
+	return nil
+}
+
+func (s *Server) IFaceName() string {
+	return s.Ctrl.Device().Name()
+}
