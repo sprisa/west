e5719c405ac61ba2a7b458e5ebeaf1a8380ec2bd
Gabe M <hey@gabe.mx>
2025-10-21 10:38:36 -0600

    snap: create db file if not exit

--- cmd/snap/snap.go
+++ cmd/snap/snap.go
@@ -37,10 +37,20 @@ func main() {
 			// This caused the permission in mkdir to be set incorrectly.
 			// https://stackoverflow.com/a/61645606/6635914
 			err = os.Chmod(configDirPath, 0777)
-			errutil.InvariantError(err, "error creating config dir")
+			errutil.InvariantError(err, "error chmod config dir")
 			l.Log.Info().Str("dir", configDirPath).Msg("Created common config dir")
-			return
 		}
+
+		// Create db file
+		_, err = os.Stat(db.DBFilePath)
+		if os.IsNotExist(err) {
+			_, err = os.Create(db.DBFilePath)
+			errutil.InvariantError(err, "error creating db file")
+			err = os.Chmod(db.DBFilePath, 0777)
+			errutil.InvariantError(err, "error chmod db file")
+			l.Log.Info().Str("path", db.DBFilePath).Msg("Created common db file")
+		}
+
 		return
 	}
 
